c9689900a10157f4bc57e6b970441819b062a341
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-01-21 09:50:30 -0600

    add success route

--- favorites/urls.py
+++ favorites/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.login),
+    path('success', views.success),
+
 ]
--- favorites/views.py
+++ favorites/views.py
@@ -8,3 +8,13 @@ import bcrypt
 
 def login(request):
     return render(request, 'index.html')
+
+def success(request):
+    if 'userid' in request.session:
+        user = User.objects.filter(id=request.session['userid'])
+        if user:
+            context = {
+                "user": user[0]
+            }
+        return render(request, 'success.html', context)
+    return redirect('/login')
